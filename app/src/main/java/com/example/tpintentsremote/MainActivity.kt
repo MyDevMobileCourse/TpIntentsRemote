fa21ca2a873ac5d8d23515abd3ab1c4e6a6a7c5b
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-17 13:34:41 +0100

    send message to second activity

--- app/src/main/java/com/example/tpintentsremote/MainActivity.kt
+++ app/src/main/java/com/example/tpintentsremote/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.tpintentsremote
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_MESSAGE = "com.example.tpintentsremote.twoactivities.extra.MESSAGE"
+    }
     private val LOG_TAG = MainActivity::class.java.simpleName
+    lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.editText_main)
     }
 
     fun launchSecondActivity(view: View?) {
         Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra(EXTRA_MESSAGE, editText.text.toString())
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/tpintentsremote/SecondActivity.kt
+++ app/src/main/java/com/example/tpintentsremote/SecondActivity.kt
@@ -1,11 +1,19 @@
 package com.example.tpintentsremote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var textMessage : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val intent = intent
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        textMessage = findViewById(R.id.text_message)
+        textMessage.text = message
+
     }
 }
