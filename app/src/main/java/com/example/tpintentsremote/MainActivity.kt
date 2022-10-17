a0b6977f45188ca5f9d368b73387d2ec9a903e82
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-17 13:53:28 +0100

    send reply to mainActivity

--- app/src/main/java/com/example/tpintentsremote/MainActivity.kt
+++ app/src/main/java/com/example/tpintentsremote/MainActivity.kt
@@ -1,29 +1,63 @@
 package com.example.tpintentsremote
 
+import android.R.attr
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_MESSAGE = "com.example.tpintentsremote.twoactivities.extra.MESSAGE"
+        const val TEXT_REQUEST = 1
     }
     private val LOG_TAG = MainActivity::class.java.simpleName
     lateinit var editText: EditText
+    lateinit var mReplyHeadTextView: TextView
+    lateinit var mReplyTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         editText = findViewById(R.id.editText_main)
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                mReplyHeadTextView.visibility = View.VISIBLE
+                mReplyTextView.text = reply
+                mReplyTextView.visibility = View.VISIBLE
+            }
+        }
     }
 
     fun launchSecondActivity(view: View?) {
         Log.d(LOG_TAG, "Button clicked!")
         val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra(EXTRA_MESSAGE, editText.text.toString())
-        startActivity(intent);
+        resultLauncher.launch(intent)
+    }
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+            mReplyHeadTextView.visibility = View.VISIBLE
+            mReplyTextView.text = reply
+            mReplyTextView.visibility = View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/tpintentsremote/SecondActivity.kt
+++ app/src/main/java/com/example/tpintentsremote/SecondActivity.kt
@@ -1,19 +1,33 @@
 package com.example.tpintentsremote
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SecondActivity : AppCompatActivity() {
+    companion object{
+        val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
+    }
     lateinit var textMessage : TextView
+    lateinit var reply : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         val intent = intent
         val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
         textMessage = findViewById(R.id.text_message)
+        reply = findViewById(R.id.editText_second);
         textMessage.text = message
 
+    }
+
+    fun returnReply(view : View){
+        val intent = intent
+        intent.putExtra(EXTRA_REPLY, reply.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
